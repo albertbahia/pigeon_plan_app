06b7ac399fa134751c031c99eb55889f29d01650
albertbahia <albert.bahia1@gmail.com>
2014-10-11 18:37:09 -0400

    added RESTful routes for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
 
+	def index
+		@users = User.all
+	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		@user.update(user_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@user = User.new(params[:id])
+		@user.destroy
+		redirect_to root_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :gender, :age, :zip_code, :street_address, :city, :state, :interest, :username, :email, :profile_type, :telephone_number, :password, :password_confirmation)
+	end
 
 end
