0cd926cb279c484f4c1532795aeec37c31bc25fd
albertbahia <albert.bahia1@gmail.com>
2014-10-11 21:13:31 -0400

    created seed data and views for User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# -------User seed data---------
+first_names = ['sheldon', 'leonard', 'penny', 'howard', 'raj', 'amy', 'bernadette']
+
+last_names = ['cooper', 'hoffstadter', 'wolowitz', 'koot', 'farrafowler']
+
+genders = ['male', 'female']
+
+ages = [20, 21, 22, 23, 24, 25]
+
+zip_codes = ['07039', '07052', '07086']
+
+street_addresses = ['21 Main Ave', '23 International Plaza', '34 National Street']
+
+cities = ['Chicago', 'New York City', 'Los Angeles', 'Austin', "Boulder"]
+
+states = ['NJ', 'NY', 'MD', 'CA', 'TX']
+
+interests = ['bars', 'festivals', 'concerts']
+
+usernames = ['superhero', 'batman', 'flash', 'green_lantern']
+
+emails = ['sheldon@cooper.com', 'leonard@hoffstadter.com', 'penny@penny.com', 'howard@wolowitz.com', 'rag@kooth.com', 'amy@farra.com', 'bernadette@berny.com']
+
+profile_types = ['business', 'normal']
+
+first_names.each do |first_name|
+	User.create({
+		first_name: first_name,
+		last_name: last_names.sample,
+		gender: genders.sample,
+		age: ages.sample,
+		zip_code: zip_codes.sample,
+		street_address: street_addresses.sample,
+		city: cities.sample,
+		state: states.sample,
+		interest: interests.sample,
+		username: usernames.sample,
+		email: emails.sample,
+		profile_type: profile_types.sample,
+		password: '123',
+		password_confirmation: '123'
+	})
+end
+
+# --------------------------------
+
