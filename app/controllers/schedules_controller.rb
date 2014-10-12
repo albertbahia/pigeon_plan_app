bbae2918689dbf97d885fd415ac61350b4e97ae7
albertbahia <albert.bahia1@gmail.com>
2014-10-12 17:01:10 -0400

    added RESTful routes for schedules controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,29 @@
 class SchedulesController < ApplcationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def schedule_params
+		params.require(:schedule).permit(:name, :creator, :public, :private)
+	end
+
 end
