09675e7ca51ae5abceeea2be6fb889b5f7c8e801
albertbahia <albert.bahia1@gmail.com>
2014-10-12 10:35:42 -0400

    created RESTful routes and event params for events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,29 @@
 class EventsController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:name, :creator, :address, :number_in_attendance, :price, :venue_name, :duration, :type, :calendar_date, :host_name, :number_males_attending, :number_females_attending)
+	end
+
 end
