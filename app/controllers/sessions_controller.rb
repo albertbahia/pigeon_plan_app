b8fef1652457677747f2091aedc3f4e345df322b
albertbahia <albert.bahia1@gmail.com>
2014-10-11 22:33:59 -0400

    created sessions controller, associated methods in application controller, and associated views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:current_user] = user.id
+			redirect_to root_path
+		else
+			flash.now[:login_error] = "That's not your email (or password)."
+			render :new
+		end
+	end
+
+	def destroy
+		sessions[:current_user] = nil
+		redirect_to login_path
+	end
+end
