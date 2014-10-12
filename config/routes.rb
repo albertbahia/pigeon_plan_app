3ef8debe514f5b23bd394e56f4fcf464ee62000f
albertbahia <albert.bahia1@gmail.com>
2014-10-11 21:51:54 -0400

    created routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :users
   resources :schedules
   resources :events
+
+  # ---Sessions---
+  get '/login' => 'sessions#new'
+  get '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/error' => 'sessions#error'
 end
