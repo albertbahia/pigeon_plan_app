803136eabfc3272492a587882b6e06f900371b81
albertbahia <albert.bahia1@gmail.com>
2014-12-06 15:12:16 -0500

    added parameter requirements to venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,13 +5,14 @@ class VenuesController < ApplicationController
 	end
 
 	def top_venues
-
+		@venues = Venue.all
+		@venues.sort_by! &:popularity_rating
 	end
 
 	private
 	# params.require(:venue).permit(:name, :street, :city, :state, :country, :latitude, :longitude, :popularity_rating)
 	def venue_params
-		params.require(:venue).permit(:name, :popularity_rating)
+		params.require(:venue).permit(:name, :street, :city, :state, :country, :latitude, :longitude, :popularity_rating)
 	end
 
 end
